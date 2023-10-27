5d9ccf74071ac2457e376d2ab8c9056876e1769a
Syed Sarfaraz Ahammed <syedsarfarazahammed@outlook.com>
2023-10-27 21:13:46 +0530

    implemented gh pages hosting

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH
+		}
 	},
 	preprocess: vitePreprocess()
 };
