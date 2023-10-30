51f1681ce99f33ecd0eb3a471a488d733d0cae6e
Syed Sarfaraz Ahammed <syedsarfarazahammed@outlook.com>
2023-10-30 23:34:45 +0530

    setup article component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,36 +4,17 @@
 	import { page } from '$app/stores';
 	import { Drawer, Button, CloseButton, Footer, FooterCopyright } from 'flowbite-svelte';
 	import { InfoCircleSolid, ArrowRightOutline } from 'flowbite-svelte-icons';
-	import { sineIn } from 'svelte/easing';
+
 	import { base } from '$app/paths';
-	let hidden1 = true;
-	let transitionParams = {
-		x: -320,
-		duration: 200,
-		easing: sineIn
-	};
-	let innerWidth;
+
 	let drawerHidden = false;
 	let activateClickOutside = false;
 	let backdrop = false;
 </script>
 
-<svelte:window bind:innerWidth />
 <div class="app-container flex flex-col w-screen h-screen">
 	<TopNav />
 	<div class="flex flex-row h-screen">
-		<!-- {#if innerWidth <= 768}
-			<div class="text-center">
-				<Button on:click={() => (hidden1 = false)}>Show drawer</Button>
-			</div>
-
-			<Drawer transitionType="fly" {transitionParams} bind:hidden={hidden1} id="sidebar1">
-				<SideNav />
-			</Drawer>
-		{:else}
-			<SideNav />
-		{/if} -->
-
 		<slot />
 	</div>
 	<Footer>
